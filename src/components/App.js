a9cab210fe82f93dc23863834e43b9841d19e253
gifdog97 <omusubset.s@gmail.com>
2021-02-03 19:33:04 +0900

    implement counter

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function App = () => {
+// state: 実体+関数
+const App = () => {
+  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('hello')
   return (
     <>
-      hello
+      { message}: { count}
+      <div>
+        <button onClick={() => setCount(cnt => ++cnt)}>+</button>
+        <button onClick={() => setCount(cnt => --cnt)}>-</button>
+        <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
+      </div>
     </>
-  );
+  )
 }
 
 export default App;
